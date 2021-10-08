5b840bd34afa0a6b049a96accf037cbd24287be2
lucaslppoi <lucaslorenzaop@gmail.com>
2021-10-08 17:48:25 -0300

    Model

---
+++ src/main/java/db1/project/product/model/Product.java
@@ -0,0 +1,58 @@
+package db1.project.product.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+public class Product {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+
+    private float price;
+
+    private int inventory;
+
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
