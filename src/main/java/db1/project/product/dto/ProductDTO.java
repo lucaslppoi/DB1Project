2382bd6ae0cb4181f17bdbf59e90cfa45d5f288f
lucaslppoi <lucaslorenzaop@gmail.com>
2021-10-08 18:32:22 -0300

    ProductDTO

---
+++ src/main/java/db1/project/product/dto/ProductDTO.java
@@ -0,0 +1,59 @@
+package db1.project.product.dto;
+
+import com.sun.istack.NotNull;
+
+public class ProductDTO {
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private float price;
+
+    @NotNull
+    private int inventory;
+
+    @NotNull
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public int getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(int inventory) {
+        this.inventory = inventory;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
